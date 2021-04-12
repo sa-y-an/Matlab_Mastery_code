b408a1f72f2c5ceda4435206f2d8903e46464e5b
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-04-13 03:29:20 +0530

    Matlab Code

---
+++ my_function.m
@@ -0,0 +1,8 @@
+function result =  my_function(inp)
+x = 4;
+result = inp + x ;
+
+% function to add the value 4 to a number that is passed to it
+% notice the use of semicolon
+% also notice while you load the function 
+% the variable x is never called into the workspace
---
+++ my_script.m
@@ -0,0 +1,6 @@
+disp('hello_world')
+X = 4;
+disp(X);
+
+% notice that nice it is a script 
+% when you call it the value X gets called into the workspace
